e6691ac988d42289c32793d7120ee69b42f98d37
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-03-03 13:52:41 -0600

    Updated users table to reflect removal of password field.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, text VARCHAR, is_done BOOLEAN, user INT);");
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR);");
+    }
+
     public static void insertItem(Connection conn, String text, int userId) throws SQLException {
 
         PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES (NULL, ?, FALSE, ?);");
@@ -43,7 +49,7 @@ public class Main {
     }
 
     public static void deleteItem(Connection conn, int id) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("DELETE * FROM todos WHERE id=?;");
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM todos WHERE id=?;");
         stmt.setInt(1, id);
         stmt.execute();
     }
@@ -58,7 +64,7 @@ public class Main {
         PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?);");
         stmt.setString(1, name);
         stmt.execute();
-    } //todo: write test
+    }
 
     public static User selectUser(Connection conn, String name) throws SQLException {
         User user = new User();
@@ -66,21 +72,19 @@ public class Main {
         stmt.setString(1, name);
         ResultSet results = stmt.executeQuery();
         if (results.next()) {
-            int id = results.getInt("user_id");
+            int id = results.getInt("id");
             user = new User(id, name);
         }
         return user;
-    } //todo: write test
+    }
 
     public static int promptMenu() {
         System.out.println("[1] - Create to-do item");
         System.out.println("[2] - Toggle to-do item");
         System.out.println("[3] - Delete to-do item");
         System.out.println("[4] - List to-do items");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your menu choice:");
-        int menuChoice = scanner.nextInt();
-        return menuChoice;
+        return captureNumber();
     }
 
     public static String captureText() {
@@ -97,28 +101,27 @@ public class Main {
         switch (input){
             case 1:
                 System.out.println("Enter your to-do item:");
-                String text = captureText();
-                insertItem(conn, text, userId);
+                insertItem(conn, captureText(), userId);
                 break;
             case 2:
                 System.out.println("Enter the number of the item you wish to toggle:");
-                int numberTog = captureNumber();
-                updateItem(conn, numberTog);
+                updateItem(conn, captureNumber());
                 break;
             case 3:
                 System.out.println("Enter the number of the item you wish to delete:");
-                int numberDel = captureNumber();
-                deleteItem(conn, numberDel);
+                deleteItem(conn, captureNumber());
                 break;
             case 4:
                 ArrayList<ToDoItem> list = selectUsersItems(conn, userId);
                 String checkbox = "[ ] ";
+                System.out.println("\nMy ToDo Items:");
                 for (ToDoItem item : list) {
                     if (item.isDone) {
                         checkbox = "[X] ";
                     }
-                    System.out.printf("FORMATTED: %s %d. %s\n", checkbox, item.id, item.text);
+                    System.out.printf("%s %d. %s\n", checkbox, item.id, item.text);
                 }
+                System.out.println("\n");
                 break;
             default:
                 System.out.println("Invalid choice.");
@@ -130,27 +133,23 @@ public class Main {
         String acctName = captureText();
         User user = selectUser(conn, acctName);
 
-        if(user==null){
+        if(user.name==null){
             insertUser(conn, acctName);
             user = selectUser(conn, acctName);
         }
         return user.id;
     }
 
-
     public static void main(String[] args) throws SQLException {
 
         Server.createWebServer().start();
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
-        Statement stmt = conn.createStatement();
-        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, text VARCHAR, is_done BOOLEAN, user INT);");
-        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);");
+        createTables(conn);
 
         int currentUserId = login(conn);
 
         while (true) {
-            int menuChoice = promptMenu();
-            processMenuChoice(conn, menuChoice, currentUserId);
+            processMenuChoice(conn, promptMenu(), currentUserId);
         }
     }
 }
